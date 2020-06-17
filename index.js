2e433c13dd495ce4791cb5d3c34b08ee40c720d4
DiamondDyer <diamond.l.dyer@gmail.com>
2020-06-17 17:57:26 -0400

    initial commit

--- index.js
+++ index.js
@@ -39,9 +39,36 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(edible) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+};
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
+
+const personOne = new Person("Noah", 21);
+
+console.log(personOne.toString());
+personOne.eat("chicken");
+personOne.eat("steak");
+personOne.eat("pork");
+console.log(personOne.stomach);
+personOne.poop();
+console.log(personOne.stomach);
+
 
 /*
   TASK 2
@@ -57,9 +84,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons;
+};
+
+const carOne = new Car("Ford", 24);
+
+carOne.fill(20);
+
+console.log(carOne.tank);
+
 
 /*
   TASK 3
@@ -68,18 +109,29 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
+
+const babyOne = new Baby("Jon", 1, "Ball");
+
+console.log(babyOne.play());
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/ Global Object Binding is when the value of the "this" keyword is the window in the console. 
+  2. Implicit Binding is when whatever is to the left of the dot is what "this" is.
+  3. New Binding makes the "this" keyword apply to the new object that is created. 
+  4. Explicit Binding makes "this" explicitly defined.
 */
 
 
